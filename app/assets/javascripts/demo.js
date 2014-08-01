b9b23bf40a64998b73d63b7c6d059524ab601169
yves <yves.tatienne@gmail.com>
2014-08-01 14:45:57 +0000

    delete pin

--- app/assets/javascripts/demo.js
+++ app/assets/javascripts/demo.js
@@ -14,130 +14,48 @@ $(document).ready(function() {
 						TweenMax.to($('#premier-nav'), 1, {css:{ top: '0', position:'fixed'}, immediateRender:true, ease:Quad.easeInOut} )
 					], .5)
 
-					
-
-
-
 				// pin element, use onPin and onUnpin to adjust the height of the element
-				controller.pin($('#first-pin'), pinDur, {
+				controller.pin($('#nav-pin'), pinDur, {
 					anim:pinAnimations, 
 					
 					onUnpin: function() {
-						$('#first-pin').css('position','relative');
+						$('#nav-pin').css('position','relative');
 					}
 				});
 
 
-controller.pin($('#examples-eng2-zero'), 600, {
-					anim: (new TimelineLite())
-						.append(
-			TweenMax.from($('#examples-eng2-zero #fly-it-one img'), 1,  {css:{right:'1000px', rotation:15 }, ease:Quad.easeInOut})
-						)
-						.append([
-			TweenMax.to($('#examples-eng2-zero #fly-it-two img'), 1,  {css:{rotation:15, marginTop:'-20%'}, ease:Quad.easeInOut})
-						], .5)
-	
-						.append([
-			TweenMax.from($('#examples-eng2-zero #fly-it-tree img'), 1, {css:{left:'1000px', rotation: -15}, ease:Quad.easeOut})
-						], .5)
-						
-						.append(
-			TweenMax.to($('#examples-eng2-zero '), 1, {css:{backgroundColor:'#df2223'}})
-						)
-					})
+controller.addTween('#examples-eng2-zero', TweenMax.to( $('#examples-eng2-zero'), .5, {css:{backgroundColor:'#df2223'}}));
+controller.addTween('#fly-it-two img', TweenMax.to( $('#fly-it-two img'), .5, {css:{rotation:15, marginTop:'-20%' , opacity: '1'}, ease:Quad.easeInOut}));
+
+controller.addTween('#fly-it-one img', TweenMax.fromTo( $('#fly-it-one img'), .5, {css:{left:'-1000px', rotation:-15, marginTop:'10%' , opacity: '1'}, ease:Quad.easeInOut}, {css:{left:'10%', rotation:15}, ease:Quad.easeInOut}));
+
+
+controller.addTween('#fly-it-tree img', TweenMax.fromTo( $('#fly-it-tree img'), .5, {css:{left:'1000px', rotation:15, opacity: '1'}, ease:Quad.easeInOut}, {css:{left:'10%', rotation:-15}, ease:Quad.easeInOut}));
 
 
 
-			controller.pin($('#examples-eng2-one'), 0, {
-					anim: (new TimelineLite())
-						.append( TweenMax.from($('#examples-eng2-one h2'), .25, {css:{color : '#df2223'}})
-						),
-						
-					onUnpin: function() {
-						$('#slade-one').css('height','800px');
-						}	
-					});
+			
+controller.addTween('#examples-eng2-tree', TweenMax.to( $('#examples-eng2-tree'), .5, {css:{backgroundColor:'#0097d0'}}));
+controller.addTween('#spin-it-one img', TweenMax.to( $('#spin-it-one img'), .5, {css:{ rotation: 15, opacity: '1'}, ease:Quad.easeOut}));
+controller.addTween('#spin-it-two img', TweenMax.to( $('#spin-it-two img'), .5, {css:{ rotation: -10, opacity: '1'}, ease:Quad.easeOut}));
 
 
+			
+controller.addTween('#examples-eng2-four', TweenMax.to( $('#examples-eng2-four'), .5, {css:{backgroundColor:'#009752'}}));
+controller.addTween( '#move-one img', TweenMax.to( $( '#move-one img'), .5, {css:{rotation: -5, opacity:'1'}, ease:Quad.easeOut}));
+controller.addTween( '#move-two img', TweenMax.from( $( '#move-two img'), .5, {css:{right:'10%', rotation: 15}, ease:Quad.easeOut}));
 
-			controller.pin($('#examples-eng2-two'), 0, {
-					anim: (new TimelineLite())
-						.append( TweenMax.from($('#examples-eng2-two h2'), .25, {css:{color : '#f3c000'}})
-						),
-						
-					onUnpin: function() {
-						$('#slade-two').css('height','800px');
-						}	
-					})
 
-			controller.pin($('#examples-eng2-tree'), 1000, {
-					anim: (new TimelineLite())
-						
-						.append(
-			TweenMax.to($('#examples-eng2-tree #spin-it-one img'), .25, {css:{opacity:1, rotation: 15}, ease:Quad.easeOut})
-						)
-						.append(
-			TweenMax.to($('#examples-eng2-tree #spin-it-two img'), .25, {css:{opacity:1, rotation: -15}, ease:Quad.easeOut})
-						)
-						.append(
-			TweenMax.to($('#examples-eng2-tree'), .25, {css:{backgroundColor:'#0097d0'}})
-						)
-					})
+controller.addTween('#examples-eng2-seven', TweenMax.to( $('#examples-eng2-seven'), .5, {css:{backgroundColor:'#f3c000'}}));		
+controller.addTween('#fly-one img', TweenMax.fromTo( $('#fly-one img'), .5, {css:{left:'-1000px', rotation:-15}, ease:Quad.easeInOut}, {css:{left:'10%', rotation:15}, ease:Quad.easeInOut}));
 
-		
 
-			controller.pin($('#examples-eng2-four'), 800, {
-					anim: (new TimelineLite())
-						.append(
-			TweenMax.to($('#examples-eng2-four'), .25, {css:{backgroundColor:'#009752'}})
-						)
-						.append(
-						TweenMax.fromTo( $(' #flyleft-eng img'), .25, {css:{ marginLeft: '-10000px'}, immediateRender:true, ease:Quad.easeInOut}, {css:{ marginLeft:'0px'}, ease:Quad.easeInOut})						
-						)
-						.append([
-							TweenMax.from($('#examples-eng2-four #move-one img'), 1, {css:{right:'1000px', rotation: 15}, ease:Quad.easeOut})
-						], .5)
-						
-					})
+controller.addTween('#examples-eng2-eight', TweenMax.to( $('#examples-eng2-eight'), .5, {css:{backgroundColor:'#df2223'}}));		
+controller.addTween('#fly-two img', TweenMax.fromTo( $('#fly-two img'), .5, {css:{left:'1000px', rotation:15}, ease:Quad.easeInOut}, {css:{left:'10%', rotation:-15}, ease:Quad.easeInOut}));
 
-			controller.pin($('#examples-eng2-five'), 0, {
-					anim: (new TimelineLite())
-						.append(
-			TweenMax.to($('#examples-eng2-five'), .25, {css:{backgroundColor:'#f3c000'}})
-						),
-						
-					onUnpin: function() {
-						$('#slade-five').css('height','600px');
-						}
-					})
+			
 
-			controller.pin($('#examples-eng2-six'), 0, {
-					anim: (new TimelineLite())
-						.append(
-			TweenMax.to($('#examples-eng2-six'), .25, {css:{backgroundColor:'yellow'}})
-						)
-					})
-
-			controller.pin($('#examples-eng2-seven'), 800, {
-					anim: (new TimelineLite())
-						.append(
-			TweenMax.from($('#examples-eng2-seven #fly-one img'), 1,  {css:{left:'-1000px', rotation:15}, ease:Quad.easeInOut})
-						)
-						.append(
-			TweenMax.to($('#examples-eng2-seven'), .25, {css:{backgroundColor:'#f3c000'}})
-						)
-						
-					})
-			controller.pin($('#examples-eng2-eight'), 800, {
-					anim: (new TimelineLite())
-						.append(
-			TweenMax.from($('#examples-eng2-eight #fly-one img'), 1,  {css:{left:'1000px', rotation:15}, ease:Quad.easeInOut})
-						)
-						.append(
-			TweenMax.to($('#examples-eng2-eight'), .25, {css:{backgroundColor:'#df2223'}})
-						)
-					})
-
+			
 			
 
 
